e46c6a1788b91dabe5c827f26b92b18f3cac8ab1
Daniel Martins <martinsda@gmail.com>
2018-11-19 16:47:35 -0200

    Update stringSplitAndJoin.js

--- learning/stringSplitAndJoin.js
+++ learning/stringSplitAndJoin.js
@@ -1,6 +1,13 @@
 let str = 'Daniel'
 
+// Daniel
 console.log(str)
+
+// ["D", "a", "n", "i", "e", "l"]
 console.log(str.split(''))
+
+// ["l", "e", "i", "n", "a", "D"]
 console.log(str.split('').reverse())
+
+// leinaD
 console.log(str.split('').reverse().join(''))
